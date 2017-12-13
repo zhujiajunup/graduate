d36da96643c1477db56d0f4291c65bfe3aa5384e
zhujiajun <jjzhu_ncu@163.com>
2017-12-13 21:06:13 +0800

    change

--- WeiboSpider/test.py
+++ WeiboSpider/test.py
@@ -1,6 +1,8 @@
 from selenium import webdriver
 from redis_cookies import RedisCookies
+from urllib.request import urlretrieve
 from time import sleep
+import requests
 # from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 # dcap = dict(DesiredCapabilities.PHANTOMJS)
 # driver = webdriver.PhantomJS(desired_capabilities=dcap)
@@ -9,21 +11,30 @@ browser.get('https://weibo.com/login.php')
 username = browser.find_element_by_id("loginname")
 username.clear()
 username.send_keys("767543579@qq.com")
+sleep(1)
 psd = browser.find_element_by_xpath('//input[@type="password"]')
 psd.clear()
 psd.send_keys("jvs7452014@jjzhu")
+sleep(1)
 commit_btn = browser.find_element_by_xpath('//a[@node-type="submitBtn"]')
 commit_btn.click()
 sleep(10)
-verify_box = browser.find_element_by_xpath('//div[@node-type="verifycode_box"]')
-if verify_box:
-    browser.save_screenshot("verify_code.png")
-    code = input("输入验证码：")
-    code_input = browser.find_element_by_xpath('//input[@node-type="verifycode"]')
-    code_input.clear()
-    code_input.send_keys(code)
-    commit_btn = browser.find_element_by_xpath('//a[@node-type="submitBtn"]')
-    commit_btn.click()
+while True:
+    verify_box = browser.find_element_by_xpath('//div[@node-type="verifycode_box"]')
+    if verify_box:
+        code_img = browser.find_element_by_xpath('//img[@node-type="verifycode_image"]')
+        img_url = code_img.get_attribute('src')
+        urlretrieve(img_url, "img.png")
+
+        browser.save_screenshot("verify_code.png")
+        code = input("输入验证码：")
+        code_input = browser.find_element_by_xpath('//input[@node-type="verifycode"]')
+        code_input.clear()
+        code_input.send_keys(code)
+        commit_btn = browser.find_element_by_xpath('//a[@node-type="submitBtn"]')
+        commit_btn.click()
+    else:
+        break
 for elem in browser.get_cookies():
     print(elem["name"], elem["value"])
 print("end")

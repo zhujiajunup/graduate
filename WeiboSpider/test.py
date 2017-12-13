a37ecfd4b1ba002cd3f790ed21b1ee43c7909a96
hzzhujiajun <hzzhujiajun@corp.netease.com>
2017-12-13 16:48:18 +0800

    add: 通过云打码获取验证码

--- WeiboSpider/test.py
+++ WeiboSpider/test.py
@@ -1,32 +1,84 @@
 from selenium import webdriver
-from redis_cookies import RedisCookies
+from selenium.common.exceptions import NoSuchElementException
+# from redis_cookies import RedisCookies
+import os
 from time import sleep
+from PIL import Image
+from code_recognize import YunDaMa
 # from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 # dcap = dict(DesiredCapabilities.PHANTOMJS)
 # driver = webdriver.PhantomJS(desired_capabilities=dcap)
 browser = webdriver.Firefox()
-browser.get('https://weibo.com/login.php')
-username = browser.find_element_by_id("loginname")
-username.clear()
-username.send_keys("767543579@qq.com")
-psd = browser.find_element_by_xpath('//input[@type="password"]')
-psd.clear()
-psd.send_keys("jvs7452014@jjzhu")
-commit_btn = browser.find_element_by_xpath('//a[@node-type="submitBtn"]')
-commit_btn.click()
-sleep(10)
-verify_box = browser.find_element_by_xpath('//div[@node-type="verifycode_box"]')
-if verify_box:
-    browser.save_screenshot("verify_code.png")
-    code = input("输入验证码：")
-    code_input = browser.find_element_by_xpath('//input[@node-type="verifycode"]')
-    code_input.clear()
-    code_input.send_keys(code)
+
+yun_da_ma = YunDaMa(username='zhujiajun', password='vs7452014')
+
+
+def save_verify_code_img():
+    screen_shot_path = 'D:\\graduate\\WeiboSpider\\img\\screenshot.png'
+    code_img_path = 'D:\\graduate\\WeiboSpider\\img\\verify_code.png'
+    browser.save_screenshot(screen_shot_path)
+    code_img = browser.find_element_by_xpath('//img[@node-type="verifycode_image"]')
+    left = code_img.location['x']
+    top = code_img.location['y']
+    width = code_img.location['x'] + code_img.size['width']
+    height = code_img.location['y'] + code_img.size['height']
+    picture = Image.open('./img/screenshot.png')
+    picture = picture.crop((left, top, width, height))
+    picture.save(code_img_path)
+    os.remove(screen_shot_path)
+    return code_img_path
+
+
+def login():
+    try_time = 10
+    browser.get('https://weibo.com/login.php')
+    username = browser.find_element_by_id("loginname")
+    username.clear()
+    username.send_keys("g6382912shanlu4@163.com")
+    psd = browser.find_element_by_xpath('//input[@type="password"]')
+    psd.clear()
+    psd.send_keys("vs7452014")
     commit_btn = browser.find_element_by_xpath('//a[@node-type="submitBtn"]')
     commit_btn.click()
-for elem in browser.get_cookies():
-    print(elem["name"], elem["value"])
-print("end")
+    # 没那么快登录成功
+    sleep(10)
+    while try_time:
+        try:
+            # 如果登录不成功是有验证码框的
+            browser.find_element_by_xpath('//div[@node-type="verifycode_box"]')
+            img_path = save_verify_code_img()
+            while not os.path.exists(img_path):
+                print(img_path + "not exist")
+                sleep(1)
+            print(img_path)
+            sleep(1)
+            captcha_id, code_text = yun_da_ma.recognize(img_path)
+            os.remove(img_path)
+            code_str = bytes.decode(code_text)
+            print('recognize result: %s' % code_str)
+            code_input = browser.find_element_by_xpath('//input[@node-type="verifycode"]')
+
+            code_input.clear()
+            code_input.send_keys(code_str)
+            commit_btn = browser.find_element_by_xpath('//a[@node-type="submitBtn"]')
+            commit_btn.click()
+            # 稍等一会
+            sleep(5)
+            try_time -= 1
+        except NoSuchElementException:
+            print('login success')
+            break
+
+    for elem in browser.get_cookies():
+        print(elem["name"], elem["value"])
+    print("end")
+
+
+def home():
+    browser.get('https://weibo.cn/1316949123/info')
+login()
+home()
+
 # cookies_json = RedisCookies.fetch_cookies()
 # # driver.delete_all_cookies()
 # cookies = cookies_json['cookies']

334a55f6224837df35a83abeb4f82a62ac9b03eb
hzzhujiajun <hzzhujiajun@corp.netease.com>
2018-01-04 18:36:48 +0800

    change: 修改邮箱

--- src/main/java/cn/edu/zju/zjj/controller/WeiboUserController.java
+++ src/main/java/cn/edu/zju/zjj/controller/WeiboUserController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
- * @author 祝佳俊(hzzhujiajun@corp.netease.com)
+ * @author 祝佳俊(zhujijunup@163.com)
  */
 @Slf4j
 @RestController

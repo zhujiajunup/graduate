d5c8e9e215e87a88b6055533fa5b4f2c3c1c046e
zhujiajun <jjzhu_ncu@163.com>
2018-03-27 23:58:33 +0800

    add: thesis

--- src/main/resources/static/js/charts/userinfo.js
+++ src/main/resources/static/js/charts/userinfo.js
@@ -13,18 +13,18 @@ export default function UserInfo(id, tid) {
 
             dom.innerHTML = '<div class="panel-body">\n' +
                 '                        <div class="clearfix text-center m-t">\n' +
-                '                            <div class="inline">\n' +
+                '                            <div class="inline" style="text-align: center;">\n' +
                 '                                <div class="easypiechart easyPieChart" data-percent="75" data-line-width="5"\n' +
                 '                                     data-bar-color="#4cc0c1" data-track-color="#f5f5f5" data-scale-color="false"\n' +
                 '                                     data-size="134" data-line-cap="butt" data-animate="1000"\n' +
-                '                                     style="width: 134px; height: 134px; line-height: 134px;">\n' +
+                '                                     style="width: 134px; height: 134px; line-height: 134px;display: inline-block;">\n' +
                 '                                    <div class="thumb-lg">\n' +
                 '                                        <img src="'+result.data['head'] +'"\n' +
                 '                                             class="img-circle" alt="...">\n' +
                 '                                    </div>\n' +
                 '                                    <canvas width="134" height="134"></canvas>\n' +
                 '                                </div>\n' +
-                '                                <div class="h4 m-t m-b-xs">'+result.data['nickname'] + '</div>\n' +
+                '                                <div class="h4 m-t m-b-xs" style="font-size: 18px;">'+result.data['nickname'] + '</div>\n' +
                 '                                <small class="text-muted m-b">'+result.data['signature'] + '</small>\n' +
                 '                            </div>\n' +
                 '                        </div>\n' +
@@ -33,19 +33,19 @@ export default function UserInfo(id, tid) {
                 '                        <div class="row pull-out">\n' +
                 '                            <div class="col-xs-4">\n' +
                 '                                <div class="padder-v">\n' +
-                '                                    <span class="m-b-xs h3 block text-white">'+followNum + '</span>\n' +
+                '                                    <span class="m-b-xs h3 block text-white" style="font-size: 18px;">'+followNum + '</span>\n' +
                 '                                    <small class="text-muted">关注</small>\n' +
                 '                                </div>\n' +
                 '                            </div>\n' +
                 '                            <div class="col-xs-4 dk">\n' +
                 '                                <div class="padder-v">\n' +
-                '                                    <span class="m-b-xs h3 block text-white">'+fansNum+ '</span>\n' +
+                '                                    <span class="m-b-xs h3 block text-white" style="font-size: 18px;">'+fansNum+ '</span>\n' +
                 '                                    <small class="text-muted">粉丝</small>\n' +
                 '                                </div>\n' +
                 '                            </div>\n' +
                 '                            <div class="col-xs-4">\n' +
                 '                                <div class="padder-v">\n' +
-                '                                    <span class="m-b-xs h3 block text-white">'+tweetNum+'</span>\n' +
+                '                                    <span class="m-b-xs h3 block text-white" style="font-size: 18px;">'+tweetNum+'</span>\n' +
                 '                                    <small class="text-muted">微博</small>\n' +
                 '                                </div>\n' +
                 '                            </div>\n' +
